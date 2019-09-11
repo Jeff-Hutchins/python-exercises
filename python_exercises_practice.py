df959d7ac6140df204ed7c73b4d432d85695a3c0
Jeffrey Hutchins <jiffy@Jeffreys-MacBook-Air.local>
2019-09-11 08:12:10 -0500

    practice

---
+++ python_exercises_practice.py
@@ -0,0 +1,81 @@
+type(99.9)
+type("False")
+type(False)
+type('0')
+type(0)
+type(True)
+type('True')
+type[{}] #list of dictionary
+type{'a':[]} #dictionary
+type(.1)
+
+# What data type would best represent:
+
+# A term or phrase typed into a search box?
+# string
+
+# If a user is logged in?
+# boolean
+
+# A discount amount to apply to a user's shopping cart?
+# float
+
+# Whether or not a coupon code is valid?
+# boolean
+
+# An email address typed into a registration form?
+# string
+
+# The price of a product?
+# float
+
+# A Matrix?
+# list
+
+# The email addresses collected from a registration form?
+# tuple
+
+# Information about applicants to Codeup's data science program?
+# dictionary
+
+# You have rented some movies for your kids: 
+# The little mermaid (for 3 days), 
+# Brother Bear (for 5 days, they love it), 
+# and Hercules (1 day, you don't know yet if they're 
+# going to like it). If price for a movie per day 
+# is 3 dollars, how much will you have to pay?
+
+the_little_mermaid_days = 3
+brother_bear_days = 5
+hercules_days = 1
+price = 3
+total_price = (the_little_mermaid_days + brother_bear_days + hercules_days) * price
+print(total_price)
+
+google_hourly_rate = 400
+amazon_hourly_rate = 380
+facebook_hourly_rate = 350
+
+pay = (google_hourly_rate * 6) + (amazon_hourly_rate * 4) + (facebook_hourly_rate * 10)
+
+print(pay)
+
+'''Illustrate input and print.'''
+
+applicant = input("Enter the applicant's name: ")
+interviewer = input("Enter the interviewer's name: ")
+time = input("Enter the appointment time: ")
+print(interviewer, "will interview", applicant, "at", time)
+
+student = input("Enter the students name: ")
+is_class_full = False
+class_schedule_conflict = False
+enroll_student != is_class_full and class_schedule_conflict
+
+enrolled = 0
+
+def(student):
+    if is_class_full = False and class_schedule_conflict = False
+    return enrolled += 1
+
+
