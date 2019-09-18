7c37a26e122ab1cbe04a1ed4df708529d1393544
Jeffrey Hutchins <jiffy@jeffreys-air.lan>
2019-09-18 04:00:56 -0500

    done

--- practice_solutions.py
+++ practice_solutions.py
@@ -1,22 +1,22 @@
-def extract_time_components(time):
-    '''
->>> extract_time_components('21:30:00')
-{'hours': 21, 'minutes': 30, 'seconds': 0}
->>> extract_time_components('09:01:53')
-{'hours': 9, 'minutes': 1, 'seconds': 53}
-    '''
-    hours, minutes, seconds = [int(n) for n in time.split(':')]
+# def extract_time_components(time):
+#     '''
+# >>> extract_time_components('21:30:00')
+# {'hours': 21, 'minutes': 30, 'seconds': 0}
+# >>> extract_time_components('09:01:53')
+# {'hours': 9, 'minutes': 1, 'seconds': 53}
+#     '''
+#     hours, minutes, seconds = [int(n) for n in time.split(':')]
 
-    time_dict = {
-        'hours': hours,
-        'minutes': minutes,
-        'seconds': seconds
-    }
+#     time_dict = {
+#         'hours': hours,
+#         'minutes': minutes,
+#         'seconds': seconds
+#     }
 
-    return time_dict
+#     return time_dict
 
     #or
-
+    # time_dict = {}
     # parts = time.split(':')
 
     # hours = parts[0]
@@ -30,4 +30,51 @@ def extract_time_components(time):
 
     # return time_dict
 
-extract_time_components('21:30:00')
+# extract_time_components('21:30:00')
+
+def keep_long_words(words):
+    '''
+    >>> keep_long_words(['ls', 'codeup', 'code', 'pip', 'bayes'])
+    ['codeup', 'bayes']
+    >>> keep_long_words(['cd', 'ls', 'pwd'])
+    []
+    >>> keep_long_words(['python', 'algorithm'])
+    ['python', 'algorithm']
+    '''
+    long_words = []
+    for word in words:
+        if len(word) > 4:
+            long_words.append(word)
+    return long_words
+
+
+def make_model(cars):
+    '''
+    >>> cars = []
+    >>> cars.append({'make': 'Toyota', 'model': 'Camry'})
+    >>> cars.append({'make': 'Honda', 'model': 'Accord'})
+    >>> cars.append({'make': 'Ford', 'model': 'Fiesta'})
+    >>> cars.append({'make': 'Ford', 'model': 'F-150'})
+    >>> make_model(cars)
+    ['Toyota Camry', 'Honda Accord', 'Ford Fiesta', 'Ford F-150']
+    '''
+    makes_and_models = []
+    for car in cars:
+        makes_and_models.append(car['make'] + ' ' + car['model'])
+    return makes_and_models
+
+def extract_time_components(s):
+    '''
+    >>> extract_time_components('21:30:00')
+    {'hours': 21, 'minutes': 30, 'seconds': 0}
+    >>> extract_time_components('09:01:53')
+    {'hours': 9, 'minutes': 1, 'seconds': 53}
+    '''
+    hours, minutes, seconds = [int(i) for i in s.split(':')]
+
+    my_dict = {
+        'hours': hours,
+        'minutes': minutes,
+        'seconds': seconds
+    }
+    return my_dict
