acf12e337d98bc3bb940f53a0e07afe16e63a38c
Jeffrey Hutchins <jeffrey.w.hutchins@gmail.com>
2020-01-08 13:43:25 -0600

    Interview questions

---
+++ white_board_problem.py
@@ -0,0 +1,6 @@
+# Repeat question
+# Write down the question
+# Confirm you write down the correct problem statement
+# Ask any clarifying questions
+# Show and tell your thinking.  Narrating your problem solving.
+# Produce the answer
