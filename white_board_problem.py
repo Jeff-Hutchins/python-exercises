d14268a45eb73d49c28fa901cec9a6903a9c5f2a
Jeffrey Hutchins <jeffrey.w.hutchins@gmail.com>
2020-02-05 21:00:48 -0600

    completed

--- white_board_problem.py
+++ white_board_problem.py
@@ -3,4 +3,4 @@
 # Confirm you write down the correct problem statement
 # Ask any clarifying questions
 # Show and tell your thinking.  Narrating your problem solving.
-# Produce the answer
+# Produce the answer and talk through each piece
